3562d89847154d3ce0730151987babf5cd1c8966
Jan Engehausen <smurf667@gmail.com>
2019-08-31 14:47:29 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+`anti-ad` is a **Firefox** add-on that removes "Ad blocking detected" messages from HTML pages. Some website overlay their message in a way that after removing them the original content can be viewed.
+
+This is just the source, as getting this installable required upload to Mozilla etc. which I am not prepared to do at this time.
+
+The add-on can be activated using [`about:debugging`](about:debugging) by loading it manually via "Load Temporary Add-on...". Open the project folder and load the `manifest.json` file.
+
+A "campfire" icon will be shown in the menu bar. Clicking it on a website it knows how to handle, the "Ad blocking detected" message will be removed.
+
+The removal code can be found in [`remove-message.js`](remove-message.js). For each website - matched via a regular expression - a handler can be registered. Example:
+
+```javascript
+{
+  regexp: new RegExp('http(s?):\/\/www\.independent\.co\.uk\/.*'),
+  remover: function() {
+    document.querySelector(".tp-modal").style="display: none";
+    document.querySelector(".tp-backdrop").style="display: none";
+  }
+}
+```
+
+Pull requests for further websites and improvements welcome.
---
+++ remove-message.js
@@ -0,0 +1,70 @@
+var handlers = [
+  {
+    regexp: new RegExp('http(s?):\/\/www\.independent\.co\.uk\/.*'),
+	remover: function() {
+	  document.querySelector(".tp-modal").style="display: none";
+	  document.querySelector(".tp-backdrop").style="display: none";
+	}
+  },
+  {
+    regexp: new RegExp('http(s?):\/\/www\.faz\.net\/.*'),
+	remover: function() {
+      [].forEach.call(
+        document.querySelectorAll('body *'), 
+        function(element){
+          var elementDisplay = element.currentStyle ? element.currentStyle.display : getComputedStyle(element, null).display;
+          if (elementDisplay === 'none') {
+            element.style = '';
+          }
+        }
+      );
+      document.querySelector("#cboxOverlay").style="display: none";		
+	}
+  },
+  {
+    regexp: new RegExp('http(s?):\/\/.+tagesanzeiger\.ch\/.*'),
+	remover: function() {
+      document.querySelector('#tamovl-main-container').style = 'display: none';
+      var x = document.getElementsByTagName('style');
+      for (i = 0; i < x.length; i++) {
+        if (x[i].innerText.indexOf('tamovl') > 0) {
+          x[i].remove();
+        }
+      }
+	}
+  },
+  {
+    regexp: new RegExp('http(s?):\/\/.+spiegel\.de\/.*'),
+	remover: function() {
+		[].forEach.call(
+			document.querySelectorAll('*'), 
+			function(element){
+				element.style = '';
+				element.class = '';
+			}
+		);
+		document.querySelector('[class^=sp_veil]').remove();
+	}
+  },
+  {
+    regexp: new RegExp('http(s?):\/\/.+zsz\.ch\/.*'),
+	remover: function() {
+		let nodes = document.querySelectorAll('*[class*="adbl"]');
+		if (nodes) {
+			nodes.forEach( n => {
+				n.parentNode.removeChild(n);
+			});
+		}
+		document.body.setAttribute("class", "");
+	}
+  }
+]
+
+var url = window.location.href;
+
+for (i = 0; i < handlers.length; i++) {
+  if (handlers[i].regexp.test(url)) {
+    handlers[i].remover();
+	break;
+  }
+}
