ac7468e2b98290d18aee193911a2ada7ce6ad678
Jan Engehausen <smurf667@gmail.com>
2019-09-01 09:58:13 +0200

    some reformatting

--- remove-message.js
+++ remove-message.js
@@ -1,29 +1,28 @@
-var handlers = [
-  {
+var handlers = [{
     regexp: new RegExp('http(s?):\/\/www\.independent\.co\.uk\/.*'),
-	remover: function() {
-	  document.querySelector(".tp-modal").style="display: none";
-	  document.querySelector(".tp-backdrop").style="display: none";
-	}
+    remover: function() {
+      document.querySelector(".tp-modal").style = "display: none";
+      document.querySelector(".tp-backdrop").style = "display: none";
+    }
   },
   {
     regexp: new RegExp('http(s?):\/\/www\.faz\.net\/.*'),
-	remover: function() {
+    remover: function() {
       [].forEach.call(
-        document.querySelectorAll('body *'), 
-        function(element){
+        document.querySelectorAll('body *'),
+        function(element) {
           var elementDisplay = element.currentStyle ? element.currentStyle.display : getComputedStyle(element, null).display;
           if (elementDisplay === 'none') {
             element.style = '';
           }
         }
       );
-      document.querySelector("#cboxOverlay").style="display: none";		
-	}
+      document.querySelector("#cboxOverlay").style = "display: none";
+    }
   },
   {
     regexp: new RegExp('http(s?):\/\/.+tagesanzeiger\.ch\/.*'),
-	remover: function() {
+    remover: function() {
       document.querySelector('#tamovl-main-container').style = 'display: none';
       var x = document.getElementsByTagName('style');
       for (i = 0; i < x.length; i++) {
@@ -31,32 +30,32 @@ var handlers = [
           x[i].remove();
         }
       }
-	}
+    }
   },
   {
     regexp: new RegExp('http(s?):\/\/.+spiegel\.de\/.*'),
-	remover: function() {
-		[].forEach.call(
-			document.querySelectorAll('*'), 
-			function(element){
-				element.style = '';
-				element.class = '';
-			}
-		);
-		document.querySelector('[class^=sp_veil]').remove();
-	}
+    remover: function() {
+      [].forEach.call(
+        document.querySelectorAll('*'),
+        function(element) {
+          element.style = '';
+          element.class = '';
+        }
+      );
+      document.querySelector('[class^=sp_veil]').remove();
+    }
   },
   {
     regexp: new RegExp('http(s?):\/\/.+zsz\.ch\/.*'),
-	remover: function() {
-		let nodes = document.querySelectorAll('*[class*="adbl"]');
-		if (nodes) {
-			nodes.forEach( n => {
-				n.parentNode.removeChild(n);
-			});
-		}
-		document.body.setAttribute("class", "");
-	}
+    remover: function() {
+      let nodes = document.querySelectorAll('*[class*="adbl"]');
+      if (nodes) {
+        nodes.forEach(n => {
+          n.parentNode.removeChild(n);
+        });
+      }
+      document.body.setAttribute("class", "");
+    }
   }
 ]
 
@@ -65,6 +64,6 @@ var url = window.location.href;
 for (i = 0; i < handlers.length; i++) {
   if (handlers[i].regexp.test(url)) {
     handlers[i].remover();
-	break;
+    break;
   }
 }
