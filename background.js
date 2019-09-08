ffd02200709def8fe1c7c161170b520e83da1fd8
Jan Engehausen <smurf667@gmail.com>
2019-09-08 18:37:52 +0200

    clean up

--- background.js
+++ background.js
@@ -1,11 +1,3 @@
-function handleClick(tab) {
-  console.log('this is tab', tab);
-  tab.executeScript({
-    file: "remove-message.js"
-  });
-// browser.tabs.executeScript({
-}
-
 browser.browserAction.onClicked.addListener(function(){
   browser.tabs.executeScript({
     file: "remove-message.js"
